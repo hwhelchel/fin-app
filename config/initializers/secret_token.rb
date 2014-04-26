01427f51171a07c597bb4922fec05b1cd4b4db6e
Harry Whelchel <harrywhelchel@gmail.com>
2014-04-26 11:01:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinApp::Application.config.secret_key_base = '986f04a97c545e70db9e821725cfb537f8b23c4619fe33aec57bd3f3511e4539f4fa2a4b1845bb6a6b86a942f44b169464b3212cd624eb241c033fcd6dc301ad'
